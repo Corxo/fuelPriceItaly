8ac56b6b527db14623a410d8a4917b2ff40f1e2a
Cristiano Venanzi <cristiano@cristianovenanzi.it>
2022-03-22 07:19:33 +0100

    added getInfo function

--- oilPriceItaly.js
+++ oilPriceItaly.js
@@ -1,13 +1,43 @@
-const tg = require('telegraf');
-const env = require('./env');
-const db = require('better-sqlite3')(env.DB_PATH);
+import { Telegraf, Telegram } from 'telegraf';
+import {TELEGRAM_KEY} from './env.js'
+import fetch from 'node-fetch';
 
-const bot = new tg.Telegraf(env.TELEGRAM_KEY);
+const bot = new Telegraf(TELEGRAM_KEY);
 
-bot.command('info',ctx=>{
-    let data = db.prepare('SELECT * FROM prices LIMIT 5').all();
-    ctx.reply(JSON.stringify(data));
-})
+const getInfo = async (lat,log)=>{
+    if(!lat || !log)
+        throw "Missing latitude or longitude";
+    let body = {"points": [{lat: lat, lng: log}]};
+    try{
+        let call = await fetch(
+            "https://carburanti.mise.gov.it/ospzApi/search/zone", {
+                "headers": {
+                    "accept": "application/json",
+                    "accept-language": "it-IT,it;q=0.9,en-GB;q=0.8,en;q=0.7",
+                    "content-type": "application/json",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-origin",
+                    "sec-gpc": "1",
+                    "cookie": "cookies_consent=true",
+                    "Referer": "https://carburanti.mise.gov.it/ospzSearch/zona",
+                    "Referrer-Policy": "strict-origin-when-cross-origin"
+                },
+                "body": JSON.stringify(body),
+                "method": "POST"
+            }
+        );
+        return call.text();
+    }
+    catch(err){
+        throw err;
+    }
+}
+
+/* async function test(){
+    console.log(await getInfo(41.890546, 12.49425));
+}
+test(); */
 
 bot.command('nearme',ctx=>{
     //GET POSITION AND RETURN CLOSEST STATIONS SORTED BY PRICE
